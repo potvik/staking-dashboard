329560a2b6e005ca4ba3e90b16cacea10b36d580
potvik <57394565+potvik@users.noreply.github.com>
2020-03-04 17:14:48 +0300

    Update index.js

--- frontend/src/filters/index.js
+++ frontend/src/filters/index.js
@@ -16,6 +16,7 @@ export const formatBech32 = (address, longForm = false, length = 4) => {
   }
 }
 
+
 export const resolveValidatorName = (address, validators) => {
   if (validators[address]) {
     return validators[address].moniker
